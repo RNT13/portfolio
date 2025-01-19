f8209c3989ec8af68b2063aabb762a0801581aec
RNT13 <renatornt13@gmail.com>
2025-01-19 15:19:49 -0300

    Criando a seção de projetos e o componente de projetos

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,13 @@
+import Text from '../Text'
+import Title from '../title'
+import { Card, LinkBotao } from './styles'
+
+const Project = () => (
+  <Card>
+    <Title>Projetos lista de tarefas</Title>
+    <Text type="Secondary">lista de tarefas com VueJS</Text>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Project
--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -1,9 +1,37 @@
+import Project from '../../components/Project'
 import Title from '../../components/title'
+import { Lista } from './styles'
 
 const Projects = () => {
   return (
     <section>
-      <Title fontSize={16}>Projects</Title>
+      <Title fontSize={16}>Projects list</Title>
+      <Lista>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+        <li>
+          <Project></Project>
+        </li>
+      </Lista>
     </section>
   )
 }
